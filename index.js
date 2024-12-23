e96ea7501f0bd541ce896014797aa1e0f4e18fd4
Shrafi Shehab <svshuvo696@gmail.com>
2024-12-23 08:39:08 +0600

    API route to delete a service conplete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -50,6 +50,14 @@ async function run() {
         app.post('/services', async (req, res) => {
             const newService = req.body;
             const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        });
+
+        // delete service
+        app.delete('/service/:id', async (req, res) => {
+            const serviceId = req.params.id;
+            const query = { _id: new ObjectId(serviceId) }
+            const result = await serviceCollection.deleteOne(query);
             res.send(result);
         });
 
